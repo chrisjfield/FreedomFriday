03e97d09851cb0eeee32ae6daf0488651e81c2f5
chris field <chris.field@theaccessgroup.com>
2018-09-28 14:16:18 +0100

    drop down bio

--- ViroSample/js/FindAccessEmployee.js
+++ ViroSample/js/FindAccessEmployee.js
@@ -11,6 +11,8 @@ import {
     ViroText,
     ViroImage,
     ViroNode,
+    ViroAnimations,
+    ViroBox,
 } from 'react-viro';
 
 var createReactClass = require('create-react-class');
@@ -21,28 +23,49 @@ var FindAccessEmployee = createReactClass({
         return {
             anchorFound: 1,
             pauseUpdates: false,
+            playPictureAnim: false,
+            showDetail: false,
         }
     },
 
     render: function () {
         return (
             <ViroARScene>
-                <ViroText text={String(this.state.anchorFound)} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-
                 <ViroARImageMarker target={"accessCard"} onAnchorFound={this._onAnchorFound} pauseUpdates={this.state.pauseUpdates}>
-                    <ViroNode position={[0.75, -2, 0]} scale={[1, 1, 1]} rotation={[265, 0, 15]} dragType="FixedToWorld" onDrag={() => { }}>
+                    <ViroNode position={[0.9, -2, 0]} scale={[1, 1, 1]} rotation={[265, 0, -75]}
+                        dragType="FixedToWorld" onDrag={() => { }} animation={{ name: "rotateImage", run: this.state.playPictureAnim, }}>
                         <ViroImage
                             source={require("./res/Ed.jpg")}
+                            onClick={this._onImageClick}
+                            Opacity="0.6"
                         />
                     </ViroNode>
+                    {this.state.showDetail
+                        ? <ViroNode position={[0.95, -2.1, 0]} scale={[1, 1, 1]} rotation={[265, 0, 15]} animation={{ name: "dropInfo", run: true, }}>
+                            {/* <ViroImage
+                                source={require("./res/accessCard.jpg")}
+                                onClick={this._onImageClick}
+                                Opacity="0.6"
+                            /> */}
+                            <ViroText text={bioText} style={styles.bioTextStyle} />
+                        </ViroNode>
+                        : null
+                    }
                 </ViroARImageMarker>
             </ViroARScene >
         );
     },
 
+    _onImageClick() {
+        this.setState({
+            showDetail: !this.state.showDetail,
+        })
+    },
+
     _onAnchorFound() {
         this.setState({
             pauseUpdates: true,
+            playPictureAnim: true,
             anchorFound: this.state.anchorFound + 1,
         })
     },
@@ -56,13 +79,32 @@ ViroARTrackingTargets.createTargets({
     },
 });
 
+ViroAnimations.registerAnimations({
+    rotateImage: {
+        properties: {
+            rotateZ: "+=90",
+        },
+        duration: 1000,
+        easing: "EaseOut",
+    },
+    dropInfo: {
+        properties: {
+            positionZ: "+=1",
+        },
+        duration: 1000,
+    },
+});
+
+var bioText = ` 
+I am Ed
+
+I love dicks
+
+nom nom nom
+`
 var styles = StyleSheet.create({
-    helloWorldTextStyle: {
-        fontFamily: 'Arial',
-        fontSize: 30,
-        color: '#FFFFFF',
-        textAlignVertical: 'center',
-        textAlign: 'center',
+    bioTextStyle: {
+        color: '#E5173F',
     },
 });
 
