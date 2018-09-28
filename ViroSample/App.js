ea28727ec7ccbfdfdbf0d6460be58413b77200b3
Ed.Burt <ed.burt@theaccessgroup.com>
2018-09-28 13:33:00 +0100

    access branding

--- ViroSample/App.js
+++ ViroSample/App.js
@@ -7,27 +7,20 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-import React, { Component } from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  TouchableHighlight,
-} from 'react-native';
+import React, { Component } from "react";
+import { Text, Image, View, StyleSheet, TouchableHighlight } from "react-native";
 
-import {
-  ViroARSceneNavigator
-} from 'react-viro';
+import { ViroARSceneNavigator } from "react-viro";
 
 /*
  TODO: Insert your API key below
  */
 var sharedProps = {
-  apiKey: "C8D76B6E-C2D9-4EBE-841A-C6BE0CDC4300",
-}
+    apiKey: "C8D76B6E-C2D9-4EBE-841A-C6BE0CDC4300"
+};
 
 // Sets the default scene you want for AR and VR
-var InitialARScene = require('./js/FindAccessEmployee');
+var InitialARScene = require("./js/FindAccessEmployee");
 
 var UNSET = "UNSET";
 var AR_NAVIGATOR_TYPE = "AR";
@@ -37,124 +30,125 @@ var AR_NAVIGATOR_TYPE = "AR";
 var defaultNavigatorType = UNSET;
 
 export default class ViroSample extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      navigatorType: defaultNavigatorType,
-      sharedProps: sharedProps
+        this.state = {
+            navigatorType: defaultNavigatorType,
+            sharedProps: sharedProps
+        };
+        this._enterAccessReality = this._enterAccessReality.bind(this);
+        this._getARNavigator = this._getARNavigator.bind(this);
+        this._getExperienceButtonOnPress = this._getExperienceButtonOnPress.bind(this);
+        this._exitViro = this._exitViro.bind(this);
     }
-    this._enterAccessReality = this._enterAccessReality.bind(this);
-    this._getARNavigator = this._getARNavigator.bind(this);
-    this._getExperienceButtonOnPress = this._getExperienceButtonOnPress.bind(this);
-    this._exitViro = this._exitViro.bind(this);
-  }
 
-  // Replace this function with the contents of _getVRNavigator() or _getARNavigator()
-  // if you are building a specific type of experience.
-  render() {
-    if (this.state.navigatorType == UNSET) {
-      return this._enterAccessReality();
-    } else if (this.state.navigatorType == AR_NAVIGATOR_TYPE) {
-      return this._getARNavigator();
+    // Replace this function with the contents of _getVRNavigator() or _getARNavigator()
+    // if you are building a specific type of experience.
+    render() {
+        if (this.state.navigatorType == UNSET) {
+            return this._enterAccessReality();
+        } else if (this.state.navigatorType == AR_NAVIGATOR_TYPE) {
+            return this._getARNavigator();
+        }
     }
-  }
 
-  // Presents the user with a choice of an AR or VR experience
-  _enterAccessReality() {
-    return (
-      <View style={localStyles.outer} >
-        <View style={localStyles.inner} >
-          <TouchableHighlight style={localStyles.buttons}
-            onPress={this._getExperienceButtonOnPress(AR_NAVIGATOR_TYPE)}
-            underlayColor={'#68a0ff'} >
-
-            <Text style={localStyles.buttonText}>Access Reality</Text>
-          </TouchableHighlight>
-        </View>
-      </View>
-    );
-  }
-
-  // Returns the ViroARSceneNavigator which will start the AR experience
-  _getARNavigator() {
-    return (
-      <ViroARSceneNavigator {...this.state.sharedProps}
-        initialScene={{ scene: InitialARScene }} />
-    );
-  }
-
-  // This function returns an anonymous/lambda function to be used
-  // by the experience selector buttons
-  _getExperienceButtonOnPress(navigatorType) {
-    return () => {
-      this.setState({
-        navigatorType: navigatorType
-      })
+    // Presents the user with a choice of an AR or VR experience
+    _enterAccessReality() {
+        return (
+            <View style={localStyles.outer}>
+                <View style={localStyles.inner}>
+                    <Image source={require("./js/res/accessLogo.png")} />
+                    <Text style={localStyles.accessText}>freedom to do more</Text>
+                    <TouchableHighlight style={localStyles.buttons} onPress={this._getExperienceButtonOnPress(AR_NAVIGATOR_TYPE)} underlayColor={"#68a0ff"}>
+                        <Text style={localStyles.buttonText}>Access Reality</Text>
+                    </TouchableHighlight>
+                    <Text style={localStyles.accessText}>#WeareAcess</Text>
+                </View>
+            </View>
+        );
     }
-  }
 
-  // This function "exits" Viro by setting the navigatorType to UNSET.
-  _exitViro() {
-    this.setState({
-      navigatorType: UNSET
-    })
-  }
+    // Returns the ViroARSceneNavigator which will start the AR experience
+    _getARNavigator() {
+        return <ViroARSceneNavigator {...this.state.sharedProps} initialScene={{ scene: InitialARScene }} />;
+    }
+
+    // This function returns an anonymous/lambda function to be used
+    // by the experience selector buttons
+    _getExperienceButtonOnPress(navigatorType) {
+        return () => {
+            this.setState({
+                navigatorType: navigatorType
+            });
+        };
+    }
+
+    // This function "exits" Viro by setting the navigatorType to UNSET.
+    _exitViro() {
+        this.setState({
+            navigatorType: UNSET
+        });
+    }
 }
 
 var localStyles = StyleSheet.create({
-  viroContainer: {
-    flex: 1,
-    backgroundColor: "black",
-  },
-  outer: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: "black",
-  },
-  inner: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    backgroundColor: "black",
-  },
-  titleText: {
-    paddingTop: 30,
-    paddingBottom: 20,
-    color: '#fff',
-    textAlign: 'center',
-    fontSize: 25
-  },
-  buttonText: {
-    color: '#fff',
-    textAlign: 'center',
-    fontSize: 20
-  },
-  buttons: {
-    height: 80,
-    width: 150,
-    paddingTop: 20,
-    paddingBottom: 20,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor: '#68a0cf',
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#fff',
-  },
-  exitButton: {
-    height: 50,
-    width: 100,
-    paddingTop: 10,
-    paddingBottom: 10,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor: '#68a0cf',
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#fff',
-  }
+    accessText: {
+        textAlign: "center",
+        fontSize: 20,
+        backgroundColor: "white",
+        color: "black",
+        padding: 20
+    },
+    viroContainer: {
+        flex: 1,
+        backgroundColor: "black"
+    },
+    outer: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "white",
+        height: "100%"
+    },
+    inner: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center"
+    },
+    titleText: {
+        paddingTop: 30,
+        paddingBottom: 20,
+        color: "#fff",
+        textAlign: "center",
+        fontSize: 25
+    },
+    buttonText: {
+        color: "#fff",
+        textAlign: "center",
+        fontSize: 20
+    },
+    buttons: {
+        height: 50,
+        width: 150,
+        paddingTop: 20,
+        paddingBottom: 60,
+        backgroundColor: "#099",
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: "#fff",
+        alignItems: "center"
+    },
+    exitButton: {
+        height: 50,
+        width: 100,
+        paddingTop: 10,
+        paddingBottom: 10,
+        backgroundColor: "#68a0cf",
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: "#fff"
+    }
 });
 
-module.exports = ViroSample
+module.exports = ViroSample;
